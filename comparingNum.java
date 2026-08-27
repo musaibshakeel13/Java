00cda2c076509f34ed1df2d05b088355b09dbc6f
Musaib shakeel <hajimusaib0@gmail.com>
2026-08-27 22:26:43 +0530

    Update comparingNum.java

--- comparingNum.java
+++ comparingNum.java
@@ -1,1 +1,24 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+import java.util.Scanner;
+
+public class numComparing {
+   public numComparing() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter First number:");
+      int var2 = var1.nextInt();
+      System.out.println("Enter second number:");
+      int var3 = var1.nextInt();
+      if (var2 == var3) {
+         System.out.println("Numbers are equal");
+      } else if (var2 > var3) {
+         System.out.println("Num1 is greater");
+      } else {
+         System.out.println("Num2 is greater");
+      }
+
+   }
+}
 
