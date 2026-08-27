230de0eb35d3c2f8f029e5d3fe609c716ca96385
Musaib shakeel <hajimusaib0@gmail.com>
2026-08-27 22:23:30 +0530

    Create checkEvenOdd.java

---
+++ checkEvenOdd.java
@@ -0,0 +1,17 @@
+// package conditionalStatements;
+
+import java.util.*;
+public class checkEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("ENter the number to check the number is even or odd:");
+        int number=sc.nextInt();
+        if (number%2==0){
+            System.out.println("Even number?");
+        }
+        else{
+            System.out.println("Odd number!");
+        }
+    }
+    
+}
