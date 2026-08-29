c6484d044173d1fc85d58c7af9dc63b57e394a00
Musaib shakeel <hajimusaib0@gmail.com>
2026-08-29 21:42:05 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,1 +1,15 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int A=sc.nextInt();
+        System.out.println("ENter the second number:");
+        int B=sc.nextInt();
+        int sum=A+B;
+        System.out.println("the sum of two numbers is: "+sum);
+    }
+}
+
+
 
