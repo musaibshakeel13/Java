541c26e1a5251dc78ae9bf10d7af769c36101219
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-01 20:16:49 +0530

    Create preIncrementandPostIncrement.java

---
+++ preIncrementandPostIncrement.java
@@ -0,0 +1,12 @@
+public class preIncrementandPostIncrement {
+    public static void main(String[] args) {
+        int p=100;
+        int q=p++;
+        int r=++p;
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(r);
+    }
+
+
+}
