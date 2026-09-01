c36ceabd2cdf8223b4f1507d14fbc4506f96021a
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-01 20:17:35 +0530

    Create booleanclass.java

---
+++ booleanclass.java
@@ -0,0 +1,15 @@
+public class booleanclass {
+    public static void main(String[] args) {
+        boolean myBoolean = false;
+        if(myBoolean==true){
+            System.out.println("your boolean is:"+myBoolean);
+            System.out.println("Hello World");
+
+
+        }
+        else {
+            System.out.println("Your system is craked bro");
+        }
+    }
+}
+
