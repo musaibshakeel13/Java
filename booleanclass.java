12d44ba14ee4772bebadcc10035b39d1a2e3803f
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-06 22:39:22 +0530

    Update booleanclass.java

--- booleanclass.java
+++ booleanclass.java
@@ -1,1 +1,16 @@
+public class booleanclass {
+    public static void main(String[] args) {
+        boolean myBoolean = false;
+        if(myBoolean==true){
+            System.out.println("your boolean is:"+myBoolean);
+            System.out.println("Hello World");
+
+
+        }
+        else {
+            System.out.println("Your system is craked bro");
+        }
+    }
+}
+
 
