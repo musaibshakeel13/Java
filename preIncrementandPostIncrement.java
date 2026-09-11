2c71fa180268cafc3d143f731e2c3bdc0425a01a
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-11 21:33:37 +0530

    Update preIncrementandPostIncrement.java

--- preIncrementandPostIncrement.java
+++ preIncrementandPostIncrement.java
@@ -1,1 +1,14 @@
+public class preIncrementandPostIncrement {
+    public static void main(String[] args) {
+        int p=100;
+        int q=p++;
+        int r=++p;
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(r);
+    }
+
+
+}
+
 
